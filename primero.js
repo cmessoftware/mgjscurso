96c3425b79de234194b6babab41333dd4298e327
cmessoftware <cmessoftware@gmail.com>
2022-09-27 21:14:32 -0300

    primer ejemplo

---
+++ primero.js
@@ -0,0 +1,22 @@
+
+//Si a > b -> sumo sino resto
+function compara(a,b)
+{
+    var r
+
+    if(a > b){
+        r = a+b
+    }else{
+        r = a-b
+    }
+
+    return r
+}
+
+
+r = compara(23,5)
+
+console.log(r)
+
+
+
