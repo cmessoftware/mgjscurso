216d0b97b45105d12d634847a3aaac9b019cdc16
GonzaloRC29 <gonzi_84_04@hotmail.com>
2022-10-12 20:34:33 -0300

    HOla

--- ejercicios/condicionales/03.js
+++ ejercicios/condicionales/03.js
@@ -7,10 +7,41 @@
 // calificacion < o calificacion > 10 : Calificacion invalida.
 
 
-function calificaciones(a)
-{ 
-    
+function calificaciones (a)
+{
+    if (a < 0)
+    {
+        return("Calificación invalida")
+    }
+      
+    else if (a <= 2)
+    {
+        return("Muy deficiente")
+
+    }
+    else if (a <= 4)
+    {
+        return("Insuficiente")
+    }
+    else if (a <= 6)  //Por que me tira error la siguiente expersión: if (a > 4 && a <=6)
+          
+    {
+        return("Suficiente")
+    }
+    else if (a <=8)
+    {
+        return("Bien")
+    }
+    else if (a <=10 && a >8)
+    {
+        return("Sobresaliente")
+    }
+    else if (a>10)
+    {
+        return("Calificacion invalida")
+    }
 
 }
 
-module.exports = calificaciones
+module.exports = {calificaciones}
+//calificaciones(-1)
